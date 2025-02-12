ebb34c5da124adb8f526e659836a8a5ef47c2999
aw0605 <101087917+aw0605@users.noreply.github.com>
2025-02-13 07:10:04 +0900

    프로젝트 세팅
    - eslint 설정
    - prettier 설치 및 설정
    - zustand 설치
    - styled-components 설치

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,29 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+  ),
+  {
+    files: ["*.ts", "*.tsx"],
+    languageOptioins: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: ["@typescript-eslint", "react"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
